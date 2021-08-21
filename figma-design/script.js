3759078eb7df76d690d0e49aa005fcaa33f88b87
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-08-21 12:43:05 -0300

    Adicionada transição na abertura do menu

--- figma-design/script.js
+++ figma-design/script.js
@@ -4,19 +4,23 @@ const navigationDrawer = document.querySelector('#navigation-drawer');
 const CLASS = {
   hidden: 'hidden',
   overflowHidden: 'overflow-hidden',
+  invisible: 'invisible',
+  opacity0: 'opacity-0',
 };
 
 let menuIsShowing = false;
 
 function toggleMenu() {
   if (menuIsShowing) {
-    navigationDrawer.classList.add(CLASS.hidden);
+    navigationDrawer.classList.add(CLASS.invisible);
+    navigationDrawer.classList.add(CLASS.opacity0);
     document.body.classList.remove(CLASS.overflowHidden);
     headerIconMenuClose.classList.add(CLASS.hidden);
     headerIconMenuOpen.classList.remove(CLASS.hidden);
     menuIsShowing = false;
   } else {
-    navigationDrawer.classList.remove(CLASS.hidden);
+    navigationDrawer.classList.remove(CLASS.invisible);
+    navigationDrawer.classList.remove(CLASS.opacity0);
     document.body.classList.add(CLASS.overflowHidden);
     headerIconMenuOpen.classList.add(CLASS.hidden);
     headerIconMenuClose.classList.remove(CLASS.hidden);
