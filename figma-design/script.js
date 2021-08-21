5076295c85f2d0363f882f91e3b3610911d3f178
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-08-21 13:08:51 -0300

    Melhorada transição na abertura do menu com animate.css

--- figma-design/script.js
+++ figma-design/script.js
@@ -4,29 +4,40 @@ const navigationDrawer = document.querySelector('#navigation-drawer');
 const CLASS = {
   hidden: 'hidden',
   overflowHidden: 'overflow-hidden',
-  invisible: 'invisible',
-  opacity0: 'opacity-0',
+  animateAnimated: 'animate__animated',
+  animateFaster: 'animate__faster',
+  animateMenuOut: 'animate__slideOutRight',
+  animateMenuIn: 'animate__slideInRight',
 };
 
 let menuIsShowing = false;
 
 function toggleMenu() {
+  // Remove o estado inicial do navigationDrawer
+  navigationDrawer.classList.remove(CLASS.hidden);
+
   if (menuIsShowing) {
-    navigationDrawer.classList.add(CLASS.invisible);
-    navigationDrawer.classList.add(CLASS.opacity0);
     document.body.classList.remove(CLASS.overflowHidden);
+    navigationDrawer.classList.remove(CLASS.animateMenuIn);
+    navigationDrawer.classList.add(CLASS.animateMenuOut);
     headerIconMenuClose.classList.add(CLASS.hidden);
     headerIconMenuOpen.classList.remove(CLASS.hidden);
     menuIsShowing = false;
   } else {
-    navigationDrawer.classList.remove(CLASS.invisible);
-    navigationDrawer.classList.remove(CLASS.opacity0);
     document.body.classList.add(CLASS.overflowHidden);
+    navigationDrawer.classList.remove(CLASS.animateMenuOut);
+    navigationDrawer.classList.add(CLASS.animateMenuIn);
     headerIconMenuOpen.classList.add(CLASS.hidden);
     headerIconMenuClose.classList.remove(CLASS.hidden);
     menuIsShowing = true;
   }
 }
 
+// Estado inicial do navigationDrawer
+navigationDrawer.classList.add(CLASS.hidden);
+navigationDrawer.classList.add(CLASS.animateAnimated);
+navigationDrawer.classList.add(CLASS.animateFaster);
+
+// Eventos de click no botão do menu
 headerIconMenuOpen.addEventListener('click', toggleMenu);
 headerIconMenuClose.addEventListener('click', toggleMenu);
